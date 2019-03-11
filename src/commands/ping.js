d1061b2263d38d8b6aae658148186a8834c37caf
Banhammer151 <careypjr1993@gmail.com>
2019-03-11 03:50:59 -0500

    quick update

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,7 +2,6 @@ exports.run = (client, message, args) => {
     message.channel.send("pong!").catch(console.error);
 };
 exports.help = {
-    command: "help",
     name: "ping",
     category: "misc",
     description: "ping",
---
+++ src/commands/userinfo.js
@@ -0,0 +1,26 @@
+const { Client, MessageEmbed } = require('discord.js');
+exports.run = async (client, message, args) => {
+    const member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    const members = message.mentions.users.first();
+    const embed = new MessageEmbed()
+      // Set the title of the field
+      .setTitle(`User Info for ${members.tag}`)
+      .setThumbnail(`${members.displayAvatarURL()}`)
+      // Set the color of the embed
+      .setColor(0xFF0000)
+      .setTimestamp()
+      .setFooter("Info Sent By Lunar Bot")
+      .addField("Server Join Date:", `${member.joinedAt}`)
+      .addField("Created", `${members.createdAt}`)
+      
+      // Set the main content of the embed
+      .setDescription('Heres the Basic Info You Asked For');
+    // Send the embed to the same channel as the message
+    await message.channel.send(embed);
+};
+exports.help = {
+    name: "userinfo",
+    category: "misc",
+    description: "Grab Information about a User",
+    usage: "userinfo @mention"
+}
