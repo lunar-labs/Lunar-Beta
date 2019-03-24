16c1a39240734dbab6fade7878bf2c3afa145686
Banhammer151 <careypjr1993@gmail.com>
2019-03-24 18:23:55 -0500

    basic info for express

--- Main/src/dash/app.js
+++ Main/src/dash/app.js
@@ -4,7 +4,32 @@ const helmet = require('helmet')
 var https = require('https');
 var fs = require('fs');
 var config = require('./config.json');
+session = require('express-session'),
+FileStore = require('session-file-store')(session);
 app.use(helmet())
+app.use(session({
+ 
+    // using FileStore with express-session
+    // as the sore method, replacing the default memory store
+    store: new FileStore({
+
+        path: './session-store'
+
+    }),
+    name: 'LunarLabs', // cookie will show up as foo site
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        // five year cookie
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 5
+    }
+}));
+app.use(function(req, res, next) {
+    res.locals.user = req.session.userdata;
+    res.locals.guilds = req.session.guilddata;
+    next();
+  });
 const oAuthClient = require('disco-oauth');
 const client = new oAuthClient(
     config.clientId,               // The Client ID
@@ -17,21 +42,29 @@ let port = config.port || 3000;
 app.set('port', port);
 
 app.get('/', (req, res)=>{
-    res.send(`<a href="${client.getAuthCodeLink()}">Click here to get started</a>`) // Getting the auth code link
-})
-
+    if(!req.session.userdata){
+    const loginlink = `${client.getAuthCodeLink()}`;
+    res.render('pages/index', {login:loginlink, link: "Login"});
+    }else{
+        res.render('pages/index', {login: '/logout', link: "logout"})
+    }   
+});
+app.get('/about', function(req, res){
+    res.render('pages/about');
+    });
 app.get('/login', async (req, res)=>{
     let code = req.query.code;
     try{
         let key = await client.getAccess(code) // Gets the access token
-
+        var userdata = req.session.userdata;
+        req.session.userdata = await client.getAuthorizedUser(key);
         //console.log(client.getAccessObject(key))    // Get the access token response (really not recommended)
 
         //console.log(await client.getAuthorizedUser(key)) // Gets /users/@me (will log in console)
         //res.send(await client.getAuthorizedUserGuilds(key))  // Gets /users/@me/guilds (shows in browser) (pretty ugly)
         
-        app.set('userdata', await client.getAuthorizedUser(key))
-        app.set('guilddata', await client.getAuthorizedUserGuilds(key))
+        
+        req.session.guilddata = await client.getAuthorizedUserGuilds(key);
         res.redirect("home")
     }
     catch(error){
@@ -39,8 +72,17 @@ app.get('/login', async (req, res)=>{
     }
 });
 app.get('/home', async (req, res)=>{
+    if(!req.session.guilddata){
+        res.redirect('/login');
+    }else{    
+    res.render('pages/home.ejs',{user: req.session.userdata, guilds: req.session.guilddata, login: '/logout', link: "logout"})
+    }
+});
+app.get('/logout', async (req, res)=>{
+    req.session.destroy(function(err) {
+        res.redirect('/');
+      })
     
-    res.render('home.ejs',{user: app.get('userdata'), guilds: app.get('guilddata')})
 });
 https.createServer({
     key: fs.readFileSync('encryption/private.key'),
